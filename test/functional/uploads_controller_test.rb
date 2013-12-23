2ea7b73ba8c35ed0f495ca3fb1a885298e48457b
luspi <lukas@photoqt.org>
2013-12-23 19:56:05 +0000

    some more unit test fixing

--- test/functional/uploads_controller_test.rb
+++ test/functional/uploads_controller_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class UploadsControllerTest < ActionController::TestCase
 	
    include Devise::TestHelpers  
-  # fixtures :keys
-
    setup do
       @upload = uploads(:one)
       @user = User.create!(
@@ -13,15 +11,13 @@ class UploadsControllerTest < ActionController::TestCase
         :password_confirmation => 'MyTestingPassword'
       )
       sign_in @user
-  #    @key = keys(:one)
    end
 	
 
   test "should get index" do
     get :index
     assert_response 200
-    # THE FOLLOWING CAUSES AND ERROR (NOT QUITE SURE WHY...)
-    # assert_not_nil assigns(:uploads)
+    assert_not_nil assigns(:uploads)
   end
 
   test "should get new" do
@@ -49,8 +45,7 @@ class UploadsControllerTest < ActionController::TestCase
 
   test "should update upload" do
     put :update, id: @upload, upload: { content_type: @upload.content_type, counter: @upload.counter, description: @upload.description, filecontent: @upload.filecontent, filename: @upload.filename, filesize: @upload.filesize, uploaddate: @upload.uploaddate }
-    # TEST FAILES WITH THE FOLLOWING NOT COMMENTED OUT -- CAN'T FIGURE OUT REASON.....
-    #  assert_redirected_to upload_path(assigns(:upload))
+    assert_redirected_to compact_path
   end
 
   test "should destroy upload" do
